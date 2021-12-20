395c3e96285292ee4780c1d98196d0f9466a6ad2
greenknight03 <whatelse0304@gmail.com>
2021-12-21 00:22:12 +0900

    wine

--- src/pages/wine.tsx
+++ src/pages/wine.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const WinePage: NextPage = () => {
   return (
     <div>
-      <h1>Wine</h1>
+    <h1>Wine 레드와인과의 연동? 실패~~~ 다음 시간에 계속~~~~~ </h1>
     </div>
   )
 }
