1f4dd54a577342961f36622be971b25e8a313144
greenknight03 <whatelse0304@gmail.com>
2021-12-21 00:30:54 +0900

    redwine

--- src/pages/wine/red.tsx
+++ src/pages/wine/red.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const WinePage: NextPage = () => {
   return (
     <div>
-    <h1>Wine</h1>
+    <h1>red Wine</h1>
     <link rel="stylesheet" href="red.tsx" />
     </div>
   )
