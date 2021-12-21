b77ff1bce34ee1b36147cd40dffec3e3fa9a5b17
greenknight03 <whatelse0304@gmail.com>
2021-12-22 02:36:39 +0900

    ✅  add beers file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d462042195417ba8"; }
+/******/ 		__webpack_require__.h = function() { return "de6d0fda592604d3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
